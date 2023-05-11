2d03bce3f195e0557b230d92673fd80f9368e6d5
gdoney <gdoney1701@gmail.com>
2023-05-11 13:31:08 -0400

    Adding support for input and input system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput playerInput;
+
+
+    public void OnCameraMove(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<Vector2>().normalized);
+    }
+
+    public void OnSelect(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Clicked");
+        }
+
+    }
+
+    public void OnZoom(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<Vector2>().normalized);
+    }
+}
