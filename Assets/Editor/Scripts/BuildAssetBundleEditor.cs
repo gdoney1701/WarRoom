269ef942f8a22237cd00e17633a1f41ec3810ff2
gdoney1701 <gdoney1701@gmail.com>
2023-06-23 16:34:15 -0400

    Adding asset bundles and build settings

---
+++ Assets/Editor/Scripts/BuildAssetBundleEditor.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using UnityEditor;
+
+public class BuildAssetBundleEditor
+{
+    [MenuItem("Tools/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+                                        BuildAssetBundleOptions.None,
+                                        EditorUserBuildSettings.activeBuildTarget);
+    }
+
+}
